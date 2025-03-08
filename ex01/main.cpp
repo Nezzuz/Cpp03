bc488774d618050e88da953ffcc33ecc52c5c1d1
Julen Leon Larrondo <jleon-la@c4r1s1.42urduliz.com>
2025-03-08 17:15:29 +0100

    Finished ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -22,6 +22,7 @@ int main(void)
 	ibon.beRepaired(5);
 	ibon.beRepaired(5);
 
+	std::cout << "Here i'll create a copy of julen with the copy constructor named iker but the subject doesn't let you change the name with a setter so it will appear the name julen" << std::endl;
 	ScavTrap iker(julen);
 	iker.beRepaired(20);
 	iker.guardGate();
